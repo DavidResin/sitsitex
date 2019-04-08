0e7a004fd35917fe0f2da72643d97b56264e0911
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-04-08 21:14:55 +0300

    Use regex for consistent song formatting

--- tools/scrape.py
+++ tools/scrape.py
@@ -1,6 +1,7 @@
 #This script scrapes laulu.wiki site for songs
 import requests
 import traceback
+import re
 
 # specify the url
 lauluwiki="https://laulu.wiki/api/v1/songs/{}/?format=json"
@@ -10,15 +11,20 @@ def as_song(song_id, name, s, tune = ""):
     if tune:
         tune = "\n\t\\tuneof{{{}}}".format(tune)
 
-    return "wiki_id{} = {} = {{".format(song_id, name) + tune + s + "\n}\n\n"
+    return "wiki{} = {} = {{".format(song_id, name) + tune + s + "\n}\n\n"
 
 def as_verse(s):
-    return "\n\\verse{\n\t" + s.replace("\r\n", "\\\\\n\t") + "\n}"
+    return "\n\\verse{\n\t" + clean_breaks(s) + "\\\\\n}"
+
+def clean_breaks(s):
+    br = "\\\\\\\n\t"
+    return re.sub("(\r)?\n", br, s)
+    #return s.replace("\r\n", br).replace("\n", br)
 
 def format_lyrics(verses):
     lyrics = ""
     for v in verses:
-        if v.strip():
+        if v and v.strip():
             lyrics += as_verse(v).replace("\n", "\n\t")
     return lyrics
 
@@ -28,8 +34,9 @@ def get_lauluwiki(song_id):
     page_response = requests.get(url, timeout=10).json()
     name = page_response['name']
     raw_lyrics = page_response['lyrics']    
-    melody = "" # page_response['melody']
-    verses = raw_lyrics.split("\r\n\r\n")
+    melody = "" # page_response['melody']   
+    #verses = raw_lyrics.split("\r\n\r\n")
+    verses = re.compile("(\r)?\n(\r)?\n").split(raw_lyrics)
 
     lyrics = format_lyrics(verses)
     
